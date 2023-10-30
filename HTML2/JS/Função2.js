75ac78c34567ed14bf685f8428b8032b49f29389
VitorMainchein <131178875+VitorMainchein@users.noreply.github.com>
2023-10-30 09:38:54 -0300

    Add files via upload

---
+++ HTML2/JS/Função2.js
@@ -0,0 +1,6 @@
+function mudacor() {
+    const fundo = document.querySelector("body")
+    const texto = document.querySelector("h1")
+    fundo.style.backgroundColor = 'black'
+    texto.style.color = 'white'
+}
